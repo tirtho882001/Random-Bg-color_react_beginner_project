df9596614650e93a06a4496f30143187d87aac0b
tirtho882001 <tirtho882001@gmail.com>
2023-07-11 09:59:46 +0530

    react beginner project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+function App() {
+
+  const handleClickFunction = (e) => {
+    const body = document.querySelector('body');
+    body.style.background = getRandomColor();
+    e.target.style.backgroundColor = getRandomColor();
+  };
+
+  function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>Random BG Color Changer</h1>
+        <button className="btn" onClick={handleClickFunction}>Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
